a25dce5734b8c7880c31fe0487431c19d2b9c451
Adryane Fernandes <edryfernandes@gmail.com>
2021-07-14 18:39:25 -0300

    Nome do usuário distante do menu

--- src/components/ProfileSideBar.js
+++ src/components/ProfileSideBar.js
@@ -7,12 +7,14 @@ function ProfileSideBar(props) {
     <Box>
       <img src={`https://github.com/${props.githubUser}.png`} alt={"avatar image"} style={{ borderRadius: "8px" }} />
       <hr />
+
       <p>
         <a className="boxLink" href={`https://github.com/${props.githubUser}`}>
           @{props.githubUser}
         </a>
       </p>
 
+      <hr />
       <AlurakutProfileSidebarMenuDefault />
     </Box>
   )
